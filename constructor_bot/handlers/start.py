15877241483ec24a9a545694852b3460e41cefd1
math-tma <math.tma1@gmail.com>
2026-09-10 21:06:05 +0500

    Update start.py

--- constructor_bot/handlers/start.py
+++ constructor_bot/handlers/start.py
@@ -1,3 +1,4 @@
+from aiogram.exceptions import TelegramBadRequest
 from aiogram import Router, F, Bot
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
